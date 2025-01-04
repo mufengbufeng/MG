f3be12aa2a64a7c3aaf90ecf4b6fc4b0b8806f39
mufengbufeng <320392063@qq.com>
2025-01-04 15:35:40 +0800

    分析器

---
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UIDemo/UIDemoEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace ET.Client
+{
+    [UIEvent(UIType.UIDemo)]
+    public class UIDemoEvent : AUIEvent
+    {
+        public override async ETTask<UI> OnCreate(UIComponent uiComponent, UILayer uiLayer)
+        {
+            try
+            {
+                string assetsName = $"Assets/Bundles/UI/Demo/UIDemo/{UIType.UIDemo}.prefab";
+                GameObject bundleGameObject =
+                        await uiComponent.Scene().GetComponent<ResourcesLoaderComponent>().LoadAssetAsync<GameObject>(assetsName);
+                GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject, uiComponent.UIGlobalComponent.GetLayer((int)uiLayer));
+                UI ui = uiComponent.AddChild<UI, string, GameObject>(UIType.UIDemo, gameObject);
+                ui.AddComponent<UIDemoViewComponent>();
+                return ui;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+                throw;
+            }
+        }
+
+        public override void OnRemove(UIComponent uiComponent)
+        {
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UIDemo/UIDemoViewComponentSystem.cs
@@ -0,0 +1,12 @@
+﻿namespace ET.Client
+{
+    [EntitySystemOf(typeof(UIDemoViewComponent))]
+    public static partial class UIDemoViewComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this ET.Client.UIDemoViewComponent self)
+        {
+            Log.Info("UIDemoViewComponent Awake");
+        }
+    }
+}
