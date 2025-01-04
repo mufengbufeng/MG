345e8d113b0b15c5dea07057bf37b874a8c2392f
mufengbufeng <320392063@qq.com>
2025-01-04 15:55:55 +0800

    导入TMP和NAV

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UIDemo/UIDemoEvent.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UIDemo/UIDemoEvent.cs
@@ -15,7 +15,7 @@ namespace ET.Client
                         await uiComponent.Scene().GetComponent<ResourcesLoaderComponent>().LoadAssetAsync<GameObject>(assetsName);
                 GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject, uiComponent.UIGlobalComponent.GetLayer((int)uiLayer));
                 UI ui = uiComponent.AddChild<UI, string, GameObject>(UIType.UIDemo, gameObject);
-                ui.AddComponent<UIDemoViewComponent>();
+                // ui.AddComponent<UIDemoViewComponent>();
                 return ui;
             }
             catch (Exception e)
